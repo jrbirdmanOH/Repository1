dafc24e3410e0af239a7ce9f357576bf5381eb49
Dave Diehl <jdiehl@fusionalliance.com>
2017-01-16 10:59:44 -0500

    First cut at program

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World");
+            ReadKey();
+        }
+    }
+}
